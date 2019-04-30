e625532434aab0c8d32a6341711db5927384c6a4
Adam Niedzwiecki <adamn8142@gmail.com>
2019-04-30 15:50:16 -0600

    Fetch movie data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>HELLO WORLD </h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies:[]
+    }
+  }
+
+  componentDidMount() {
+    this.fetchMovies()
+  }
+
+  fetchMovies = () => {
+    const url = "https://swapi.co/api/films"
+      fetch(url)
+        .then(response => response.json())
+        .then(results => this.setState({
+          movies: results
+        }))
+        .catch(error => console.log(error))
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Swapi Box </h1>
+      </div>
+    )
+  }
 }
 
 export default App;

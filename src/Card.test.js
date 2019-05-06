23eaf144e993f728ab12fd7bfea4b420a019391b
Adam Niedzwiecki <adamn8142@gmail.com>
2019-05-06 09:31:59 -0600

    Add tests

---
+++ src/Card.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card'
+import { shallow } from 'enzyme';
+
+describe('Card', ()=> {
+  let wrapper 
+  beforeEach(()=> {
+    wrapper = shallow (
+      <Card />
+    )
+  })
+
+  it('should match snapshot', ()=> {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
